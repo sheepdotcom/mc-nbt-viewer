f28845d59f2b8185e92c0fad48ed1365c0488923
sheepdotcom <141453189+sheepdotcom@users.noreply.github.com>
2026-07-29 15:36:15 +0000

    Deploying to gh-pages from @ sheepdotcom/mc-nbt-viewer@319d04af6ff0b7ec2a043f23079c80d60c2d2351 🚀

---
+++ sw.js
@@ -0,0 +1,41 @@
+const BUILD_TIME = 1785339372;
+const CACHE_NAME = 'mc_nbt_viewer_'+BUILD_TIME;
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './initializer.js',
+  './mc_nbt_viewer.js',
+  './mc_nbt_viewer_bg.wasm',
+];
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
